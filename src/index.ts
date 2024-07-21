20cf7887eef2f91daef823dc4d72ce50b1111d7a
工藤明哉 <kudoakiya@team-lab.com>
2024-07-21 21:44:11 +0900

    forked exifly

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import {hello} from './module.js';
-
-const hellohello = () => {
-    console.log("testing");
-    console.log(hello);     
-    return undefined;
-}
-
-export default hellohello;
+/**
+ * from  winjeysong Exifly Version 0.1.2
+ * @see https://github.com/winjeysong/exifly
+ */
+export * from './helpers.ts';
+export { default as load } from './Exifly.ts';
+export * from './tags.ts';
+export * from './typings.ts';
